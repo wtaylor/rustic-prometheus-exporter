3af73eb219b34752c64bc4a9a30e072ab153865a
William Taylor <me@willtaylor.info>
2026-04-10 08:58:03 +0100

    ci: add crates.io publishing

--- scripts/publish.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-RELEASE_VERSION=$1
-
-docker image tag rust-prometheus-operator:local ghcr.io/wtaylor/rust-prometheus-operator:latest
-docker image tag rust-prometheus-operator:local ghcr.io/wtaylor/rust-prometheus-operator:$RELEASE_VERSION
-
-docker push -a ghcr.io/wtaylor/rust-prometheus-operator
-cargo publish
