fa3f8e4a8cb9471b9073c6c3f9df818818126951
William Taylor <me@willtaylor.info>
2026-04-16 18:48:55 +0100

    feat: add snapshot metrics

--- src/actors/metrics_exporter/mod.rs
+++ src/actors/metrics_exporter/mod.rs
@@ -7,8 +7,24 @@ use crate::actors::metrics_exporter::messages::RepositoryRef;
 
 pub mod messages;
 
+pub struct SnapshotMetricStore {
+    pub started: Gauge,
+    pub finished: Gauge,
+    pub duration: Gauge,
+    pub size_total_bytes: Gauge,
+    pub size_added_bytes: Gauge,
+    pub files_new: Gauge,
+    pub files_changed: Gauge,
+    pub files_unmodified: Gauge,
+    pub files_total: Gauge,
+    pub dirs_new: Gauge,
+    pub dirs_changed: Gauge,
+    pub dirs_unmodified: Gauge,
+    pub dirs_total: Gauge,
+}
+
 pub struct RepositoryMetricStore {
-    pub _common_labels: Vec<(String, String)>,
+    pub common_labels: Vec<(String, String)>,
     pub total_snapshots: Gauge,
     pub check_success: Gauge,
     pub scrape_duration_seconds: Gauge,
@@ -16,6 +32,8 @@ pub struct RepositoryMetricStore {
     pub uncompressed_size_total: Gauge,
     pub compression_ratio: Gauge,
     pub blob_count_total: Gauge,
+
+    pub last_snapshot_store: Option<SnapshotMetricStore>,
 }
 
 impl RepositoryMetricStore {
@@ -28,8 +46,33 @@ impl RepositoryMetricStore {
             uncompressed_size_total: gauge!("restic.uncompressed_size_total", common_labels),
             compression_ratio: gauge!("restic.compression_ratio", common_labels),
             blob_count_total: gauge!("restic.blob_count_total", common_labels),
-            _common_labels: common_labels.clone(),
+            last_snapshot_store: None,
+            common_labels: common_labels.clone(),
         }
+    }
+
+    pub fn get_or_init_snapshot_store(&mut self) -> &SnapshotMetricStore {
+        if self.last_snapshot_store.is_some() {
+            return self.last_snapshot_store.as_ref().unwrap();
+        }
+
+        self.last_snapshot_store = Some(SnapshotMetricStore {
+            started: gauge!("restic.snapshot.started", &self.common_labels),
+            finished: gauge!("restic.snapshot.finished", &self.common_labels),
+            duration: gauge!("restic.snapshot.duration", &self.common_labels),
+            size_added_bytes: gauge!("restic.snapshot.size_added_bytes", &self.common_labels),
+            size_total_bytes: gauge!("restic.snapshot.size_total_bytes", &self.common_labels),
+            files_new: gauge!("restic.snapshot.files_new", &self.common_labels),
+            files_changed: gauge!("restic.snapshot.files_changed", &self.common_labels),
+            files_unmodified: gauge!("restic.snapshot.files_unmodified", &self.common_labels),
+            files_total: gauge!("restic.snapshot.files_total", &self.common_labels),
+            dirs_new: gauge!("restic.snapshot.dirs_new", &self.common_labels),
+            dirs_changed: gauge!("restic.snapshot.dirs_changed", &self.common_labels),
+            dirs_unmodified: gauge!("restic.snapshot.dirs_unmodified", &self.common_labels),
+            dirs_total: gauge!("restic.snapshot.dirs_total", &self.common_labels),
+        });
+
+        self.last_snapshot_store.as_ref().unwrap()
     }
 }
 
